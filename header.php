27397bd378f8c570ac6469b5850803710267f1b8
Mohammad Rajib Ahmed <154042825+rajibahmedguru@users.noreply.github.com>
2024-10-29 19:35:46 +0600

    add header file

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php 
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes();?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <title><?php bloginfo('name');?></title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+ 
+
+    <?php wp_head();?>
+</head>
